8b930eefe32584bb4542ff6dfbfc51ba952f6ebd
Your Name <you@example.com>
2022-02-12 17:02:04 +0800

    基础环境搭建

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
+import VueLazyload from 'vue-lazyload'
 
 import App from './App'
+import router from './router'
+import store from './store'
+
+Vue.use(VueLazyload, {
+  loading: require('./assets/loading.png')
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
+  store
 }).$mount('#app')
