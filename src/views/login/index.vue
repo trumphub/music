3098a14feea86f0a797aa76ce68bdc33a00c8a33
Your Name <you@example.com>
2022-03-01 18:26:59 +0800

    nothing

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>Login</div>
+  <transition name="move" appear>
+    <div>Login</div>
+  </transition>
 </template>
 
 <script>
 export default {
   name: "Login",
   mounted() {
-    this.$store.dispatch("login", {
-      phone: "15954027738",
-      password: "1596321oO",
-    });
+    // this.$store.dispatch("login", {
+    //   phone: "15954027738",
+    //   password: "1596321oO",
+    // });
   },
 };
 </script>
